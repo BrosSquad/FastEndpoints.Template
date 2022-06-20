46b37fc95c923c7167fc0878601b34418a8626a6
Stefan Bogdanovic <sbogdanovic@nanointeractive.com>
2022-06-20 17:52:00 +0200

    API finished

---
+++ src/FastEndpointsAPI/Endpoints/IndexEndpoint/Endpoint.cs
@@ -0,0 +1,23 @@
+namespace FastEndpointsAPI.Endpoints;
+
+public class IndexResponse
+{
+    public string Message { get; set; } = default!;
+}
+
+public class IndexEndpoint : EndpointWithoutRequest<IndexResponse>
+{
+    public override void Configure()
+    {
+        Get("/");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await SendOkAsync(new IndexResponse
+        {
+            Message = "Hello Fast Endpoints"
+        }, ct);
+    }
+}
---
+++ src/FastEndpointsAPI/Extensions/ValidationExtensions.cs
@@ -0,0 +1,28 @@
+using FluentValidation.Results;
+
+namespace FastEndpointsAPI.Extensions;
+
+public class ValidationResponse
+{
+    public string Property { get; init; } = default!;
+    public string Message { get; init; } = default!;
+}
+
+public static class ValidationExtensions
+{
+    public static List<ValidationResponse> ToResponse(this IEnumerable<ValidationFailure> errors)
+    {
+        var list = new List<ValidationResponse>();
+
+        foreach (var error in errors)
+        {
+            list.Add(new ValidationResponse
+            {
+                Property = error.PropertyName,
+                Message = error.ErrorMessage
+            });
+        }
+
+        return list;
+    }
+}
